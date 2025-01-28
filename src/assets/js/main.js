b4e0250aa77f5a106f78a6e788534eff76d778c0
masud001 <madhnagar@gmail.com>
2025-01-29 00:03:27 +0600

    header menu done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,4 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
 console.log("Hello World");
